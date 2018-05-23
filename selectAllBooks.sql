3b0c43f1ccba9bc034e222d1adaac7fe9d5fcc6e
Lucas Campos-Davis <camposdl@oregonstate.edu>
2018-05-23 16:24:22 -0700

    added comments

--- selectAllBooks.sql
+++ selectAllBooks.sql
@@ -1,4 +1,4 @@
 SELECT *
 From Media M, Book B
 WHERE M.mediaID = B.mediaID
-GROUP BY M.mediaID;
+GROUP BY M.mediaID; -- remove if we want all copies
--- selectAllFilms.sql
+++ selectAllFilms.sql
@@ -1,4 +1,4 @@
 SELECT *
 From Media M, Film F
 WHERE M.mediaID = F.mediaID
-GROUP BY M.mediaID;
+GROUP BY M.mediaID;-- remove if we want all copies
